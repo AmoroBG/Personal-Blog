d317abda772386543e367400a0acc8c4bdcad3c6
AmoroBG <amorobg@gmail.com>
2021-10-04 16:33:15 -0700

    add header and footer

--- app.js
+++ app.js
@@ -2,14 +2,23 @@ const express = require("express")
 
 const app = express()
 
+// Global Variables
+const firstPost = "This is the first post in my blog"
+
+// Public Folder
+app.use(express.static("public"))
+
 // body-parser
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+// EJS
+app.set("view engine", "ejs")
+
 // Routes
 
 app.get("/", function(req, res) {
-    res.send("Hello")
+    res.render("home.ejs", { firstPost: firstPost })
 })
 
 // Server
