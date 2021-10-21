0d453b76a007a4ace5447121bb0585a51551b0a5
Amoro Bagei <amoro.bagei@amalitech.org>
2021-10-21 15:11:32 -0700

    add menu links render other pages - about, contact

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 
 // Global Variables
 const firstPost = "This is the first post in my blog"
+const aboutContent="This is our About. - We are creative and inspiring"
+const contactContent="Contact us via mail and slack"
 
 // Public Folder
 app.use(express.static("public"))
@@ -15,13 +17,23 @@ app.use(express.urlencoded({ extended: true }))
 // EJS
 app.set("view engine", "ejs")
 
-// Routes
+// ROUTES
 
+// Home 
 app.get("/", function(req, res) {
     res.render("home.ejs", { firstPost: firstPost })
 })
+// About
+app.get("/about", function(req, res){
+    res.render("about.ejs", {aboutContent:aboutContent})
+})
 
-// Server
+// Contact
+app.get("/contact", function(req, res){
+    res.render("contact.ejs", {contactContent:contactContent})
+})
+
+// STARTING THE SERVER
 
 const PORT = 3000 | process.env.PORT
 app.listen(PORT, function() {
